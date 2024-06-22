a02f7856c05a7727b5a84fe08e3de1a1a08ae390
carlus <122530402+carlusjuniu@users.noreply.github.com>
2024-06-22 15:20:00 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {            
+      fontFamily: {
+        LondonBoutique: ['Brother Deluxe', 'sans-serif'],},
+    },
+  },
+  plugins: [],
+}
